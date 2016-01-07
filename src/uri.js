484cbb6f62e319be9052afb4039c9621e09601a6
zhangmh <zhangmhao@gmail.com>
2016-01-07 15:43:55 +0800

    Polyfill Object.assign

--- src/uri.js
+++ src/uri.js
@@ -4,6 +4,29 @@
 
 let globalParams = {};
 
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
+
 /** uri parser */
 function parseUri (str) {
     var o   = parseUri.options,
